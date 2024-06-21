9fefaa7741f433920b8e0b8ad7f0081b343ea5af
Aaron Sala <salaaron2@gmail.com>
2024-06-21 17:11:38 +0300

    learning liner codiny and es8

--- memory.js
+++ memory.js
@@ -1,11 +1,31 @@
-const person = {
-    name: "Aaron",
-    ae: 30,
-    proffesion:"programmer"
-}
-console.table(person)
+const arr = [2, 5, 8,4,12,15, 9]
 
-let arr = [1, 2, 2, 5, 6, 8, 6, 4]
+let square = arr.map(num => num * num)
 
-let neArr = [...new Set(arr)]
-console.log(neArr);
+console.log(square)
+
+const even = arr.filter(num => num % 2 === 0)
+const old = arr.filter((num) => num % 2 !== 0);
+console.log(even)
+console.log(old)
+
+const age = 30;
+age < 18 ? console.log('under age') : age > 18 ? console.log('you are welcome') : console.log("grow first")
+
+const string = "sewes"
+
+const palidrone = string.split('').reverse()
+//string===palidrone?console.log("this a palidrone"):console.log('not palidrone');
+
+const user = { name: "jane", age: 30 , email:"sala@gmail.com"}
+const hasEmail = "email" in user;
+console.log(hasEmail);
+
+const obj = { a: 1, b: 2, c: 3, d: 4 }
+console.log(Object.values(obj));
+console.log(Object.keys(obj));
+console.log(Object.entries(obj));
+
+
+const descriptors = Object.getOwnPropertyDescriptors(obj);
+console.log(descriptors);
