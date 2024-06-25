cd374def3d218a4063425f4b6ade874e3f536ef6
Aaron Sala <salaaron2@gmail.com>
2024-06-25 10:48:53 +0300

    a simple function which creates boxes as per the user input of the number of boxes they want

--- memory.js
+++ memory.js
@@ -1,31 +1,28 @@
-const arr = [2, 5, 8,4,12,15, 9]
+const conntainer = document.getElementById("container");
+const numberofInput = document.querySelector(".input");
+const button = document.querySelector(".button");
 
-let square = arr.map(num => num * num)
+function createBoxes(numberOfBoxes) {
+  //to clear any existing boxes
+  conntainer.innerHTML = "";
+  
 
-console.log(square)
+  for (let i = 0; i < numberOfBoxes; i++) {
+    //create a new div for each box
+    const box = document.createElement("div");
+    box.className = "box";
 
-const even = arr.filter(num => num % 2 === 0)
-const old = arr.filter((num) => num % 2 !== 0);
-console.log(even)
-console.log(old)
-
-const age = 30;
-age < 18 ? console.log('under age') : age > 18 ? console.log('you are welcome') : console.log("grow first")
-
-const string = "sewes"
-
-const palidrone = string.split('').reverse()
-//string===palidrone?console.log("this a palidrone"):console.log('not palidrone');
-
-const user = { name: "jane", age: 30 , email:"sala@gmail.com"}
-const hasEmail = "email" in user;
-console.log(hasEmail);
-
-const obj = { a: 1, b: 2, c: 3, d: 4 }
-console.log(Object.values(obj));
-console.log(Object.keys(obj));
-console.log(Object.entries(obj));
+    //set some content for the box
+    box.textContent = `Box ${i + 1}`;
+    conntainer.appendChild(box);
+  }
+}
 
 
-const descriptors = Object.getOwnPropertyDescriptors(obj);
-console.log(descriptors);
+button.addEventListener("click", () => {
+  
+  console.log("forming tables");
+    console.log(numberofInput.value);
+    numberOfBoxes = numberofInput.value;
+    createBoxes(numberOfBoxes);
+});
